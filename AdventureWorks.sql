9d8f8a514c1be6f9b759f1e07c544b0aea7c19b2
Abena Donkor <108335529+AbAnalytics1@users.noreply.github.com>
2023-11-10 16:01:57 +0000

    Sql Update

--- AdventureWorks.sql
+++ AdventureWorks.sql
@@ -2,3 +2,19 @@ CREATE DATABASE Adventure_Works;
 USE Adventure_Works;
 
 -- Next create tables
+-- Creating customer Table
+
+CREATE TABLE CUSTOMERS(
+	CustomerID			INT NOT NULL,
+    Firstname			VARCHAR(25),
+    Lastname			VARCHAR(25),
+    MaritalStatus		VARCHAR(25),
+    Gender				VARCHAR(10),
+    Email_Address		VARCHAR(25),
+    AnnualIncome		DECIMAL,
+    TotalChildren		SMALLINT,
+    EducationLevel		VARCHAR(25),
+    Occupation			VARCHAR(25),
+    HomeOwner			VARCHAR(25)
+    
+);
